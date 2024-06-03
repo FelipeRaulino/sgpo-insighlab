07eed9c0ee6eff12f9ea94484cacec885d199e43
Felipe Raulino <felipe.raaulino29@gmail.com>
2024-06-03 15:17:03 -0300

    chore: reinsert swagger into project

--- backend/src/main/java/com/insightlab/sgpo/config/OpenAPIConfig.java
+++ backend/src/main/java/com/insightlab/sgpo/config/OpenAPIConfig.java
@@ -1,2 +1,26 @@
-package com.insightlab.sgpo.config;public class OpenAPI {
+package com.insightlab.sgpo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI().info(new Info()
+                .title("RESTfull API made with Java 21 and Spring Boot 3")
+                .version("v1")
+                .termsOfService("https://www.apache.org/licenses/LICENSE-2.0")
+                .description("An API with a basic CRUD of persons")
+                .license(new License()
+                        .name("Apache 2.0")
+                        .url("https://www.apache.org/licenses/LICENSE-2.0")
+                )
+        );
+    }
+
 }
--- backend/src/main/java/com/insightlab/sgpo/utils/swagger/SupplierControllerSwaggerOptions.java
+++ backend/src/main/java/com/insightlab/sgpo/utils/swagger/SupplierControllerSwaggerOptions.java
@@ -1,2 +1,139 @@
-package com.insightlab.sgpo.utils.swagger;public class SupplierControllerSwaggerOptions {
+package com.insightlab.sgpo.utils.swagger;
+
+import com.insightlab.sgpo.data.dtos.v1.supplier.SupplierResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+public class SupplierControllerSwaggerOptions {
+    @Target({ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Operation(
+            summary = "Find a list of suppliers",
+            description = "Find all suppliers",
+            tags = { "Supplier" },
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = SupplierResponseDTO.class))
+                                    )
+                            }
+                    ),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    public @interface GetAllSuppliersOptions{}
+
+    @Target({ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Operation(
+            summary = "Finds a list of persons by name",
+            description = "Finds a list of persons that matches a specific name or string",
+            tags = { "People" },
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = SupplierResponseDTO.class))
+                                    )
+                            }
+                    ),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    public @interface GetAllPersonsByNameOptions{}
+
+    @Target({ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Operation(
+            summary = "Find a specific supplier",
+            description = "Find a supplier by their id.",
+            tags = { "Supplier" },
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = SupplierResponseDTO.class))
+                    ),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    public @interface GetSupplierOptions{}
+
+    @Target({ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Operation(
+            summary = "Add a new supplier",
+            description = "Add a supplier by passing info via JSON.",
+            tags = { "Supplier" },
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = SupplierResponseDTO.class))
+                    ),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    public @interface CreateSupplierOptions{}
+
+    @Target({ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Operation(
+            summary = "Update an existent supplier",
+            description = "update a supplier by passing info via JSON.",
+            tags = { "Supplier" },
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = SupplierResponseDTO.class))
+                    ),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    public @interface UpdateSupplierOptions{}
+
+    @Target({ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Operation(
+            summary = "Delete a specific supplier",
+            description = "Delete a supplier by passing id.",
+            tags = { "Supplier" },
+            responses = {
+                    @ApiResponse(
+                            description = "No Content",
+                            responseCode = "204",
+                            content = @Content
+                    ),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    public @interface DeleteSupplierOptions{}
 }
