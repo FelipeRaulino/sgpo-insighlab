d68e89694d87b4b131859a5df1126935f6d5739b
Felipe Raulino <felipe.raaulino29@gmail.com>
2024-06-08 23:46:59 -0300

    chore(docs): add JWT authorization support in Swagger documentation for secured endpoints

--- backend/src/main/java/com/insightlab/sgpo/config/OpenAPIConfig.java
+++ backend/src/main/java/com/insightlab/sgpo/config/OpenAPIConfig.java
@@ -1,17 +1,30 @@
 package com.insightlab.sgpo.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenAPIConfig {
 
+    private SecurityScheme createAPIKeyScheme(){
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+
     @Bean
     public OpenAPI openAPI(){
-        return new OpenAPI().info(new Info()
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info()
                 .title("RESTfull API made with Java 21 and Spring Boot 3")
                 .version("v1")
                 .termsOfService("https://www.apache.org/licenses/LICENSE-2.0")
